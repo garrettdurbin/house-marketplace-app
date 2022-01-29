89ef30af2344e183ed0b75c04e7ed6b1cb321061
garrettdurbin <garrettdurbin@gmail.com>
2022-01-29 10:42:47 -0600

    Fetch Data for Single Listing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -109,7 +109,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
-      location = address
       console.log(geolocation, location)
     }
 
@@ -166,9 +165,9 @@ function CreateListing() {
       timestamp: serverTimestamp()
     }
 
+    formDataCopy.location = address
     delete formDataCopy.images
     delete formDataCopy.address
-    location && (formDataCopy.location = location)
     !formDataCopy.offer && delete formDataCopy.discountedPrice
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
