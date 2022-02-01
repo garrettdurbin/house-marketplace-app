1bd52385d0dd10de6f294344f3844ee256c5abd2
garrettdurbin <garrettdurbin@gmail.com>
2022-02-01 14:45:55 -0600

    Load More Pagination

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -17,6 +17,8 @@ import ListingItem from '../components/ListingItem'
 function Category() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
 
   const params = useParams()
 
@@ -37,6 +39,9 @@ function Category() {
         // Exectute query
         const querySnap = await getDocs(q)
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+
         const listings = []
 
         querySnap.forEach((doc) => {
@@ -56,6 +61,43 @@ function Category() {
     fetchListings()
   }, [params.categoryName])
 
+  // Pagination / Load More
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, 'listings')
+
+      // Create a query
+      const q = query(
+        listingsRef,
+        where('type', '==', params.categoryName),
+        orderBy('timestamp', 'desc'),
+        startAfter(lastFetchedListing),
+        limit(10)
+      )
+
+      // Exectute query
+      const querySnap = await getDocs(q)
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+      setLastFetchedListing(lastVisible)
+
+      const listings = []
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data()
+        })
+      })
+
+      setListings((prevState) => [...prevState, ...listings])
+      setLoading(false)
+    } catch (error) {
+      toast.error('Could not fetch listings')
+    }
+  }
+
   return (
     <div className='category'>
       <header>
@@ -66,7 +108,9 @@ function Category() {
         </p>
       </header>
 
-      {loading ? <Spinner /> : listings && listings.length > 0 ?
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
         <>
           <main>
             <ul className="categoryListings">
@@ -79,9 +123,21 @@ function Category() {
               ))}
             </ul>
           </main>
-        </> : <p>No listings for {params.categoryName}</p>}
+
+          <br />
+          <br />
+
+          {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
     </div>
-  );
+  )
 }
 
-export default Category;
+export default Category
