bfd08ca529efaea3f0bd4c593879731a5991e9d2
garrettdurbin <garrettdurbin@gmail.com>
2022-02-02 15:18:16 -0600

    Added Portfolio Button to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+import { ReactComponent as HomeIcon } from '../assets/svg/homeIcon.svg'
 
 function Navbar() {
   const navigate = useNavigate()
@@ -17,6 +18,12 @@ function Navbar() {
     <footer className="navbar">
       <nav className="navbarNav">
         <ul className="navbarListItems">
+          <a href="https://garrettdurbin.com/work.html">
+            <li className="navbarListItem" onClick={() => navigate('/')}>
+              <HomeIcon fill='#2c2c2c' width='36px' height='36px' />
+              <p className='navbarListItemNameActive'>Portfolio</p>
+            </li>
+          </a>
           <li className="navbarListItem" onClick={() => navigate('/')}>
             <ExploreIcon fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px' />
             <p className={pathMatchRoute('/') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Explore</p>
